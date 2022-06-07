cdf3074ec69a72e60dd77d7473bdaee0d8c0be19
Ashish Jayamohan <46698969+ashishjayamohan@users.noreply.github.com>
2022-06-07 16:33:05 +0000

    common comparison function

--- main.py
+++ main.py
@@ -6,26 +6,30 @@ bird2 - subtle difference
 bird3 - different bird, same setting
 '''
 
+def comp(p1, p2, p1open, p2open):
+  if (p1open.size[0] != p2open.size[0] or p1open.size[1] != p2open.size[1]):
+    return -1
+  diffscore = 0
+  for i in range(p1open.size[0]):
+    for j in range(p1open.size[1]):
+      diffscore += (p1[i,j][0] - p2[i,j][0] + p1[i,j][1] - p2[i,j][1] + p1[i,j][2] - p2[i,j][2])/3
+  return diffscore
+
 p1open = Image.open("bird1.jpg")
 p2open = Image.open("bird2.jpg")
 p3open = Image.open("bird3.jpg")
-p4open = Image.open("bg.png")
 
 p1 = p1open.load()
 p2 = p2open.load()
 p3 = p3open.load()
-p4 = p4open.load()
 
 diffscore1 = 0
 diffscore2 = 0
-diffscore3 = 0
 
-for i in range(p4open.size[0]):
-  for j in range(p4open.size[1]):
+for i in range(p1open.size[0]):
+  for j in range(p1open.size[1]):
     diffscore1 += (p1[i,j][0] - p2[i,j][0] + p1[i,j][1] - p2[i,j][1] + p1[i,j][2] - p2[i,j][2])/3
     diffscore2 += (p1[i,j][0] - p3[i,j][0] + p1[i,j][1] - p3[i,j][1] + p1[i,j][2] - p3[i,j][2])/3
-    diffscore3 += (p1[i,j][0] - p4[i,j][0] + p1[i,j][1] - p4[i,j][1] + p1[i,j][2] - p4[i,j][2])/3
 
 print(str(diffscore1/(p1open.size[0]*p1open.size[1])))
 print(str(diffscore2/(p1open.size[0]*p1open.size[1])))
-print(str(diffscore3/(p1open.size[0]*p1open.size[1])))
